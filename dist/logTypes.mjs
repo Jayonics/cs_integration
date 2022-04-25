abfc7aa22b33e839712c19d6a77edbe0fbf5f11c
Jayonics <jameshopwood1998+github@gmail.com>
2022-04-25 22:16:12 +0100

    Move unused logTypes.

--- dist/logTypes.mjs
+++ dist/logTypes.mjs
@@ -1,13 +1,1 @@
-var Player = /** @class */ (function () {
-    function Player() {
-    }
-    return Player;
-}());
-export { Player };
-var Damage = /** @class */ (function () {
-    function Damage() {
-    }
-    return Damage;
-}());
-export { Damage };
 //# sourceMappingURL=logTypes.mjs.map
---
+++ dist/netcon/logTypes.mjs
@@ -0,0 +1,13 @@
+var Player = /** @class */ (function () {
+    function Player() {
+    }
+    return Player;
+}());
+export { Player };
+var Damage = /** @class */ (function () {
+    function Damage() {
+    }
+    return Damage;
+}());
+export { Damage };
+//# sourceMappingURL=logTypes.mjs.map
